6de9abb1b11e445e749cc1e836c2081fb68cf05a
Don-Stevenson <dsteven1@gmail.com>
2020-07-22 14:36:16 -0400

    basic driver data setup and basic render of info therein

--- src/App.js
+++ src/App.js
@@ -1,10 +1,59 @@
-import React from 'react';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
+
+
+// my database for drivers and tasks
+
+let drivers = [
+  {
+    id: 1,
+    name: "Omar D.",
+    tasks: [
+      {
+        pickup: { location: "", time: "" },
+        dropOff: { location: "", time: "" },
+        other: ""
+      }
+    ]
+  },
+  {
+    id: 2,
+    name: "Cheryl S.",
+    task: [
+      {
+        pickup: { location: "", time: "" },
+        dropOff: { location: "", time: "" },
+        other: ""
+      }
+    ]
+  },
+  {
+    id: 3,
+    name: "Fatima R.",
+    task: [
+      {
+        pickup: { location: "", time: "" },
+        dropOff: { location: "", time: "" },
+        other: ""
+      }
+    ]
+  }
+];
+
+console.log("drivers are: ", drivers)
 
 function App() {
   return (
     <div className="App">
-     <h1> Rose Rocket Dispatch Scheduling App </h1>
+      <h1> Rose Rocket Dispatch Scheduling App </h1>
+      {drivers.map(value => {
+        return (
+          <h2>
+            {" "}
+        {value.name} {value.id}
+          </h2>
+        );
+      })}
     </div>
   );
 }
