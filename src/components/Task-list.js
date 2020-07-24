f6fc2982fd67ef51042e8db4b701c1392526959e
Don-Stevenson <dsteven1@gmail.com>
2020-07-23 23:09:23 -0400

    display task id

--- src/components/Task-list.js
+++ src/components/Task-list.js
@@ -2,15 +2,16 @@ import React from "react";
 
 export default function TaskList({ tasks }) {
   console.log("tasks are: ", tasks);
-  // using map to display a list of tasks
+  // using map to display a list of tasks and task information
   const tasksList = () => {
     return tasks.map(task => {
-      console.log("task is", task);
+      // console.log("task is", task);
       return (
         <li key={task._id}>
           <div>
             Driver: {task.driverName.first} {task.driverName.last}{" "}
           </div>
+          <div>Task No: {task._id}</div>
           <div>Type: {task.type}</div>
           <div>Date: {task.date}</div>
           <div>Start time: {task.startTime}</div>
