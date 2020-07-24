61c2e3f2fc2cceab07d0a9b972f663241783121d
Don-Stevenson <dsteven1@gmail.com>
2020-07-23 21:25:06 -0400

    add some fake data for testing before server api is hooked up

--- src/pages/Driver-list-page.js
+++ src/pages/Driver-list-page.js
@@ -1,6 +1,38 @@
 import React from 'react';
 import DriverList from '../components/Driver-list';
 
+// test data to make sure that the page is displaying data properly
+// ***********************************************
+const data = [
+    {
+      _id: '1',
+      name: {
+        first: 'JBill',
+        last: 'Doe',
+      },
+      task: {
+        type: "drop off",
+        date: "july 24 2020",
+        startTime: "8:00",
+        stopTime: "12:00"
+     },
+    },
+    {
+      _id: '2',
+      name: {
+        first: 'Bruce',
+        last: 'Wayne',
+      },
+      task: {
+        type: "pick up",
+        date: "july 24 2020",
+        startTime: "13:00",
+        stopTime: "17:00"
+     },
+    },
+  ];
+
+
 const DriversListPage = () => {
   return (
     <div>
