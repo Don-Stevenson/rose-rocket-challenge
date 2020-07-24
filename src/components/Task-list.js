ba03e11a96e146142bf138872073fa9f2ad5b284
Don-Stevenson <dsteven1@gmail.com>
2020-07-24 01:03:06 -0400

    first sucessful call to the backend

--- src/components/Task-list.js
+++ src/components/Task-list.js
@@ -5,10 +5,10 @@ import TaskCard from './Task-card';
 export default function TaskList({ tasks }) {
   // console.log("tasks are: ", tasks);
   // using map to display a list of tasks and task information
-  console.log("in tasklist", tasks)
+  //console.log("in tasklist", tasks)
   const cards = () => {
     return tasks.map(task => {
-      console.log("task is", task)
+      // console.log("task is", task)
       return <TaskCard key={task._id} task={task} />;
     });
   };
--- src/pages/Task-list-page.js
+++ src/pages/Task-list-page.js
@@ -1,33 +1,34 @@
 import React, { useContext, useEffect } from "react";
 import TaskList from "../components/Task-list";
 import { TaskContext } from "../context/Task-context";
+import axios from "axios";
 
 // test data to make sure that the page is displaying data properly
 // ***********************************************
-const data = [
-  {
-    _id: "1",
-    driverName: {
-      first: "Bill",
-      last: "Hobson"
-    },
-    type: "drop off",
-    date: "july 24 2020",
-    startTime: "8:00",
-    stopTime: "12:00"
-  },
-  {
-    _id: "2",
-    driverName: {
-      first: "Bruce",
-      last: "Wayne"
-    },
-    type: "pick up",
-    date: "july 24 2020",
-    startTime: "13:00",
-    stopTime: "17:00"
-  }
-];
+// const data = [
+//   {
+//     _id: "1",
+//     driverName: {
+//       first: "Bill",
+//       last: "Hobson"
+//     },
+//     type: "drop off",
+//     date: "july 24 2020",
+//     startTime: "8:00",
+//     stopTime: "12:00"
+//   },
+//   {
+//     _id: "2",
+//     driverName: {
+//       first: "Bruce",
+//       last: "Wayne"
+//     },
+//     type: "pick up",
+//     date: "july 24 2020",
+//     startTime: "13:00",
+//     stopTime: "17:00"
+//   }
+// ];
 
 const TasksListPage = () => {
   // state handling using useContext
@@ -35,10 +36,15 @@ const TasksListPage = () => {
 
   // using useEffect to get driver info
   useEffect(() => {
-    dispatch({
+    const fetchData = async () => {
+      const response = await axios.get('http://localhost:3030/rr-api');
+      console.log("response is", response.data.data)
+      dispatch({
       type: "FETCH_TASKS",
-      payload: data
+      payload: response.data.data || response.data // in case pagination is disabled
     });
+    }
+    fetchData()
   }, [dispatch]);
 
  // console.log("in the task list, tasks are ", state.tasks);
