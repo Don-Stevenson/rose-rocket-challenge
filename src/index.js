cfa1af31b13a5f922364f8e2803a07cf152ee854
Don-Stevenson <dsteven1@gmail.com>
2020-07-23 20:51:39 -0400

    encapsulate root componnet with driverContextprovider

--- src/index.js
+++ src/index.js
@@ -1,16 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { BrowserRouter } from 'react-router-dom';
-import './index.css';
-import App from './App';
-import 'fomantic-ui-css/semantic.min.css';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter } from "react-router-dom";
+import "./index.css";
+import App from "./App";
+import "fomantic-ui-css/semantic.min.css";
+import * as serviceWorker from "./serviceWorker";
+import { DriverContextProvider } from "./context/Driver-context";
 
 ReactDOM.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
-  document.getElementById('root')
+  <DriverContextProvider>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+    </DriverContextProvider>,
+  document.getElementById("root")
 );
 
 // If you want your app to work offline and load faster, you can change
